6e0e759334d02bc4056c85ced3fe7938cb9b8161
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-08-29 15:44:43 +0300

    Add function declarations

--- lib/strfunc/strfunc.cpp
+++ lib/strfunc/strfunc.cpp
@@ -1,1 +1,45 @@
 #include "strfunc.h"
+
+size_t string_length(const char *str)
+{
+    size_t i = 0;
+    while((str[i++]))
+        {}
+    return i - 1u; /* don't count NUL-terminator */
+}
+
+char *string_copy(char *to, const char *from)
+{
+    while ((*(to++) = *(from++)))
+        {}
+    return to;
+}
+
+char *find_char(char *str, char c)
+{
+    for (; *str != c && *str != '\0'; str++)
+        {}
+    if (*str == c)
+        return str;
+    return NULL;
+}
+
+int compare_strings(const char *str1, const char *str2)
+{
+    while (*str1 != '\0'
+        && *str2 != '\0'
+        && (*(str1) == *(str2)))
+        {
+            str1++;
+            str2++;
+        }
+    return *str1 - *str2;
+}
+
+char *string_concat(char *to, const char *from)
+{
+    while ((*(to++)))
+        {}
+    to--;
+    return string_copy(to, from);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "strfunc.h"
 
 int main()
 {
-    puts("Hello, world!");
+    char str1[] = "ABCDEF";
+    char str2[] = "ABCD";
+    char *str3 = (char *) calloc(1, sizeof(str1) + sizeof(str2));
+    string_copy(str3, str1);
+    string_concat(str3, str2);
+    printf("%s + %s = %s\n", str1, str2, str3);
     return 0;
 }
